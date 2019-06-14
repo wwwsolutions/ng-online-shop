a2760604d9f9e2a992dd8dd7fab5ac2c87671251
Domagoj-Mario Mendas <developer.domagoj.mendas@gmail.com>
2019-06-14 11:52:51 +0200

    firebase hosting configured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBVBARm39Gzk1XGxAg4rKmHeg7QxeVfELs",
+    authDomain: "ng-online-shop.firebaseapp.com",
+    databaseURL: "https://ng-online-shop.firebaseio.com",
+    projectId: "ng-online-shop",
+    storageBucket: "",
+    messagingSenderId: "888103309644",
+    appId: "1:888103309644:web:a094aafd724d8147"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBVBARm39Gzk1XGxAg4rKmHeg7QxeVfELs",
+    authDomain: "ng-online-shop.firebaseapp.com",
+    databaseURL: "https://ng-online-shop.firebaseio.com",
+    projectId: "ng-online-shop",
+    storageBucket: "",
+    messagingSenderId: "888103309644",
+    appId: "1:888103309644:web:a094aafd724d8147"
+  }
 };
 
 /*
